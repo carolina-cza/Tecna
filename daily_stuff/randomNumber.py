79062a52c68fd081827ccef7614f99a3dc65eb5e
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2023-12-08 21:35:16 +0100

    randomNumber

---
+++ daily_stuff/randomNumber.py
@@ -0,0 +1,14 @@
+import random
+
+number = random.randint(1, 10)
+guess = None
+
+while guess != number:
+    guess = input("Guess a number between 1 and 10: ") #gibt als input eine zahl ein
+    guess =int(guess)
+
+    if guess == number: # vergleicht den Input mit der random Zahl
+        print("Congartulations! You won:)")
+        break
+    else:
+        print("Wrong. Try again!")
