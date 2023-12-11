217acf57fba554eb2a54413b3fefcb835bb4a665
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2023-12-11 16:25:23 +0100

    strings mit int verbinden

---
+++ daily_stuff/math.py
@@ -0,0 +1,13 @@
+x = min(5, 10, 25)
+y = max(5, 10, 20)
+a = abs(-7.25)
+b = pow(4,3)
+
+#variante eins um string mit int zu verbinden
+print("1: " +str(x))
+print("2: " +str(y))
+print("3: " +str(a))
+
+#variante zwei
+u = "4: "
+print(f"{u}{b}")
