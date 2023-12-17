c044db2503d213bbd4f58fdb104f7ddb39e0cffc
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2023-12-17 19:51:50 +0100

    save bc vs terminal is down

--- daily_stuff/Exception/ubungEingabe.py
+++ daily_stuff/Exception/ubungEingabe.py
@@ -3,15 +3,23 @@ import re
 name = input("Enter your name: ")
 if not re.match(r"[A-Za-z]+$",name): #erlaubt nur buchstaben als input
     print("Only letters are allowed") #error meldung wenn anderes als buchstabe eingegeben wird
-
+if  len(name)>8:
+    # try:
+    #     name = str(name)
+    # except ValueError as e:
+    print("Error only 8 charackters are allowed.")
 
 id = input("Enter your ID: ")
-try:
-    id = int(id)
-except ValueError:
-    print("Invalid input. Only numbers are allowed.") #error meldung wenn was anderes als zahlen eingegebn werden
 
-if name != "Caro" and id != "1234":
+if len(id) < 4:
+    try:
+        id = int(id)
+    except ValueError:
+        print("Invalid input. Only numbers are allowed.") #error meldung wenn was anderes als zahlen eingegebn werden
+
+    print("Mininum lenght is four. ")    
+
+if name != "Caro" and id != "20032005":
     print("Hello unknown")
 else:
     print("Welcome Back")
