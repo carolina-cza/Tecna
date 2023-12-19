48bc264e480b8b9d74b7206d117d45fd82d7989e
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2023-12-19 22:19:25 +0100

    regex teil 2

--- daily_stuff/RegEX/match.group.py
+++ daily_stuff/RegEX/match.group.py
@@ -1,8 +1,10 @@
 import re
 
 zahl = '39801 356, 2102 1111'
+#three digit number followed by space followed by two digit number
 pattern = '(\d{3}) (\d{2})'
 
+# the group() method returns the part of the string where there is a match
 match = re.search(pattern,zahl)
 
 if match:
--- daily_stuff/RegEX/re.search.py
+++ daily_stuff/RegEX/re.search.py
@@ -2,6 +2,7 @@ import re
 
 satz = "Informatik ist toll"
 
+#the re.search() method takes two arguments: a pattern and a string. The method looks for the first laocation where the Regex pattern produces a match with the string.
 match = re.search('matik', satz)
 
 if match:
