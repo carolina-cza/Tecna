071bcdb5a85b06fe744d1d49b1dd406ebabde433
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2023-12-27 14:50:49 +0100

    stuff ausprobiert

--- daily_stuff/tkinter/RauchenIstTödlich.py
+++ daily_stuff/tkinter/RauchenIstTödlich.py
@@ -4,18 +4,29 @@ window = tk.Tk()
 
 #Fenstergröße erhalten
 window.geometry("400x300")
+frage = tk.LabelFrame(window,text = "Rauchst du?")
+frage.pack()
+check1 = tk.Checkbutton(frage)
+check1["text"] = "JA"
+check1.pack()
 
-# Textausgabe erzeugen
-label1 = tk.Label(window, text="Rauchen ist tödlich",
-                  fg = 'white', #Vordergrundfarbe
-                  bg ='red', #Hintergrundfarbe
-                  font = ('arial',25,'bold','italic'),#Schriefart und Schrieftgröße setzen
-                  height=20,
-                  width=30,
-                  anchor= 'center') #ausrichtung
+check2 = tk.Checkbutton(frage)
+check2["text"] = "NEIN"
+check2.pack()
 
-#print(dir(tk.Grid))
-# in GUI Elemente einbetten
-label1.pack()
+if check1:
+    # Textausgabe erzeugen
+    label1 = tk.Label(window, text="Rauchen ist tödlich",
+                    fg = 'white', #Vordergrundfarbe
+                    bg ='red', #Hintergrundfarbe
+                    font = ('arial',25,'bold','italic'),#Schriefart und Schrieftgröße setzen
+                    height=20,
+                    width=30,
+                    anchor= 'center') #ausrichtung
 
+    #print(dir(tk.Grid))
+    # in GUI Elemente einbetten
+    label1.pack()
+else:
+    lable2 =tk.Label(window, text ="sehr gut")
 window.mainloop()
