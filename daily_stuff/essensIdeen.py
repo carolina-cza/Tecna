555e1bd3536b1e40b8ecaee5b57fb368e5bf7cf5
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2024-01-23 11:15:42 +0100

    adding some new stuff

--- daily_stuff/essensIdeen.py
+++ daily_stuff/essensIdeen.py
@@ -14,13 +14,28 @@ if frage == "healthy":
    select_healthy = random.choice(healthy)
    print(select_healthy)
 
+
 if frage == "unhealthy":
    selcet_unhealthy = random.choice(unhealthy)
    print(selcet_unhealthy)
 
 if frage == "snack":
-   select_snack = random.choice(snack)  
+   select_snack = random.choice(snack)
    print(select_snack)
 
-else:
-   print("Try again")
+# else:
+#    print("Try again")
+
+#ask for the recipe
+ask = input("Do you want the recipe? yes/no ")
+
+if ask == "yes":
+   if select_healthy == "appel cinamon oats":
+      print(" Recipe: 1 apple, 1/2 cup oats, 1tsp cinamon")
+   if select_healthy == "Banana pancakes":
+      print("Recipe: 1 bannana mash, 1 cup oats, 1 egg, 1tsp milk")
+   if select_healthy == "Chilaquiles Rojos":
+      print("Ask google pls")
+      
+if ask == "no":
+   print("ok")
