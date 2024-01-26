b3cdcadf418df5de16a5b319988bcbed728d69b3
carolina-cza <152898637+carolina-cza@users.noreply.github.com>
2024-01-26 18:18:48 +0100

    easy API code

--- daily_stuff/pygame/WorkingWithText.py
+++ daily_stuff/pygame/WorkingWithText.py
@@ -3,17 +3,17 @@ import pygame
 pygame.font.init()
 pygame.font.get_init()
 
-display_surface = pygame.display.set_mode((500,500))
+display_surface = pygame.display.set_mode((500,500),pygame.RESIZABLE)
 
 pygame.display.set_caption('Text')
 
-font1 = pygame.font.SysFont("freesanbold.ttf",50)
-text1 = font1.render("Hello World",True, (0,225,0))
+font1 = pygame.font.SysFont("Comic Sans MS",50)
+text1 = font1.render("Hello World",True, (188,172,144))
 textRect1 = text1.get_rect()
-textRect1.center= (250,250)
+textRect1.center = (250,250)
 
 while True:
-    display_surface.fill((255,0,0))
+    display_surface.fill((255,110,74))
 
     display_surface.blit(text1, textRect1)
 
